ad7154240b23dc0cf99041eccd6706235dd6055a
asd <dsa>
2025-05-09 13:43:08 -0400

    Added main method and function to parse books

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(filepath):
+    contents = ""
+    with open(filepath) as f:
+        contents = f.read()
+
+    return contents
+
+def main():
+    print(get_book_text("./books/frankenstein.txt"))
+
+main()
